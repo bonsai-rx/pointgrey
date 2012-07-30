f88fe9f579a1e10144a7eb4c23634953eb480f17
glopes <glopes@localhost>
2012-07-30 20:20:48 +0100

    Simplified all existing source implementations using the new Source contract.

--- Bonsai.PointGrey/FlyCapture.cs
+++ Bonsai.PointGrey/FlyCapture.cs
@@ -7,6 +7,8 @@ using FlyCapture2Managed;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Threading;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
 
 namespace Bonsai.PointGrey
 {
@@ -15,62 +17,8 @@ namespace Bonsai.PointGrey
         ManagedCamera camera;
         ManagedImage image;
         ManagedImage convertedImage;
-        Thread captureThread;
-        volatile bool running;
-        ManualResetEventSlim stop;
 
         public int Index { get; set; }
-
-        void CaptureNewFrame()
-        {
-            camera.StartCapture();
-
-            while (running)
-            {
-                camera.RetrieveBuffer(image);
-                if (image.pixelFormat == FlyCapture2Managed.PixelFormat.PixelFormatMono8)
-                {
-                    unsafe
-                    {
-                        var bitmapHeader = new IplImage(new CvSize((int)image.cols, (int)image.rows), 8, 1, new IntPtr(image.data));
-                        var output = new IplImage(bitmapHeader.Size, bitmapHeader.Depth, bitmapHeader.NumChannels);
-                        Core.cvCopy(bitmapHeader, output);
-                        Subject.OnNext(output);
-                    }
-                }
-                else
-                {
-                    image.Convert(FlyCapture2Managed.PixelFormat.PixelFormatBgr, convertedImage);
-
-                    var bitmap = convertedImage.bitmap;
-                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-                    try
-                    {
-                        var bitmapHeader = new IplImage(new CvSize(bitmap.Width, bitmap.Height), 8, 3, bitmapData.Scan0);
-                        var output = new IplImage(bitmapHeader.Size, bitmapHeader.Depth, bitmapHeader.NumChannels);
-                        Core.cvCopy(bitmapHeader, output);
-                        Subject.OnNext(output);
-                    }
-                    finally { bitmap.UnlockBits(bitmapData); }
-                }
-            }
-
-            camera.StopCapture();
-            stop.Set();
-        }
-
-        protected override void Start()
-        {
-            running = true;
-            captureThread = new Thread(CaptureNewFrame);
-            captureThread.Start();
-        }
-
-        protected override void Stop()
-        {
-            running = false;
-            stop.Wait();
-        }
 
         public override IDisposable Load()
         {
@@ -83,17 +31,54 @@ namespace Bonsai.PointGrey
 
             image = new ManagedImage();
             convertedImage = new ManagedImage();
-            stop = new ManualResetEventSlim();
             return base.Load();
         }
 
         protected override void Unload()
         {
-            stop.Dispose();
             convertedImage.Dispose();
             image.Dispose();
             camera.Disconnect();
             base.Unload();
+        }
+
+        protected override IObservable<IplImage> Generate()
+        {
+            return Observable.Using(
+                () =>
+                {
+                    camera.StartCapture();
+                    return Disposable.Create(camera.StopCapture);
+                },
+                resource => ObservableCombinators.GenerateWithThread<IplImage>(observer =>
+                {
+                    camera.RetrieveBuffer(image);
+                    if (image.pixelFormat == FlyCapture2Managed.PixelFormat.PixelFormatMono8)
+                    {
+                        unsafe
+                        {
+                            var bitmapHeader = new IplImage(new CvSize((int)image.cols, (int)image.rows), 8, 1, new IntPtr(image.data));
+                            var output = new IplImage(bitmapHeader.Size, bitmapHeader.Depth, bitmapHeader.NumChannels);
+                            Core.cvCopy(bitmapHeader, output);
+                            observer.OnNext(output);
+                        }
+                    }
+                    else
+                    {
+                        image.Convert(FlyCapture2Managed.PixelFormat.PixelFormatBgr, convertedImage);
+
+                        var bitmap = convertedImage.bitmap;
+                        var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                        try
+                        {
+                            var bitmapHeader = new IplImage(new CvSize(bitmap.Width, bitmap.Height), 8, 3, bitmapData.Scan0);
+                            var output = new IplImage(bitmapHeader.Size, bitmapHeader.Depth, bitmapHeader.NumChannels);
+                            Core.cvCopy(bitmapHeader, output);
+                            observer.OnNext(output);
+                        }
+                        finally { bitmap.UnlockBits(bitmapData); }
+                    }
+                }));
         }
     }
 }
