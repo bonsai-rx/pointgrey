b406164588861ccbda49d670af6c6712a387737d
glopes <glopesdev@users.noreply.github.com>
2014-02-05 00:57:55 +0000

    Added NuGet package definition for PointGrey and updated project to be AnyCPU.

--- Bonsai.PointGrey/Properties/AssemblyInfo.cs
+++ Bonsai.PointGrey/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bonsai.PointGrey")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bonsai - PointGrey Library")]
+[assembly: AssemblyDescription("Bonsai Library containing modules for acquiring images from PointGrey cameras.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gonçalo Lopes")]
 [assembly: AssemblyProduct("Bonsai.PointGrey")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo C. Lopes 2012-2013")]
+[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2012-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-ctp2")]
