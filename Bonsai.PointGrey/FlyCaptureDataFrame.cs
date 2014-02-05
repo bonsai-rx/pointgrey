89981a925bc9619f00450d55448b83adb7a0ece1
glopes <glopes@localhost>
2014-02-05 12:08:14 +0000

    Updated FlyCapture to output both image data and metadata including GPIO pins.

---
+++ Bonsai.PointGrey/FlyCaptureDataFrame.cs
@@ -0,0 +1,23 @@
+﻿using FlyCapture2Managed;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.PointGrey
+{
+    public class FlyCaptureDataFrame
+    {
+        public FlyCaptureDataFrame(IplImage image, ImageMetadata metadata)
+        {
+            Image = image;
+            Metadata = metadata;
+        }
+
+        public IplImage Image { get; private set; }
+
+        public ImageMetadata Metadata { get; private set; }
+    }
+}
---
+++ Bonsai.PointGrey/GpioPinState.cs
@@ -0,0 +1,27 @@
+﻿using FlyCapture2Managed;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.PointGrey
+{
+    public class GpioPinState : Transform<ImageMetadata, Scalar>
+    {
+        public override IObservable<Scalar> Process(IObservable<ImageMetadata> source)
+        {
+            return source.Select(input =>
+            {
+                var gpio = input.embeddedGPIOPinState;
+                var gpio0 = ((gpio >> 31) & 0x1);
+                var gpio1 = ((gpio >> 30) & 0x1);
+                var gpio2 = ((gpio >> 29) & 0x1);
+                var gpio3 = ((gpio >> 28) & 0x1);
+                return new Scalar(gpio0, gpio1, gpio2, gpio3);
+            });
+        }
+    }
+}
