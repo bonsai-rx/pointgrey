651b4ef9dcfb21aa1b6942588d088968e16415e5
glopes <glopes@localhost>
2014-02-06 03:13:42 +0000

    Updated GpioPinState to operate directly on FlyCaptureDataFrame.

--- Bonsai.PointGrey/GpioPinState.cs
+++ Bonsai.PointGrey/GpioPinState.cs
@@ -9,13 +9,13 @@ using System.Threading.Tasks;
 
 namespace Bonsai.PointGrey
 {
-    public class GpioPinState : Transform<ImageMetadata, Scalar>
+    public class GpioPinState : Transform<FlyCaptureDataFrame, Scalar>
     {
-        public override IObservable<Scalar> Process(IObservable<ImageMetadata> source)
+        public override IObservable<Scalar> Process(IObservable<FlyCaptureDataFrame> source)
         {
             return source.Select(input =>
             {
-                var gpio = input.embeddedGPIOPinState;
+                var gpio = input.Metadata.embeddedGPIOPinState;
                 var gpio0 = ((gpio >> 31) & 0x1);
                 var gpio1 = ((gpio >> 30) & 0x1);
                 var gpio2 = ((gpio >> 29) & 0x1);
