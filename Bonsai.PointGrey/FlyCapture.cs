8413b3bf7179da6347851b7157877dfd2252e523
glopes <glopes@localhost>
2017-02-08 19:50:07 +0000

    Updated FlyCapture module to use the task parallel library.

--- Bonsai.PointGrey/FlyCapture.cs
+++ Bonsai.PointGrey/FlyCapture.cs
@@ -7,86 +7,93 @@ using FlyCapture2Managed;
 using System.Threading;
 using System.Reactive.Linq;
 using System.Reactive.Disposables;
+using System.Threading.Tasks;
 
 namespace Bonsai.PointGrey
 {
     public class FlyCapture : Source<FlyCaptureDataFrame>
     {
         IObservable<FlyCaptureDataFrame> source;
+        readonly object captureLock = new object();
 
         public FlyCapture()
         {
             ColorProcessing = ColorProcessingAlgorithm.Default;
-            source = Observable.Create<FlyCaptureDataFrame>(observer =>
+            source = Observable.Create<FlyCaptureDataFrame>((observer, cancellationToken) =>
             {
-                ManagedCamera camera;
-                ManagedImage image;
-                using (var manager = new ManagedBusManager())
+                return Task.Factory.StartNew(() =>
                 {
-                    var guid = manager.GetCameraFromIndex((uint)Index);
-                    camera = new ManagedCamera();
-                    camera.Connect(guid);
-                }
-
-                var running = true;
-                var colorProcessing = ColorProcessing;
-                image = new ManagedImage();
-                var thread = new Thread(() =>
-                {
-                    camera.StartCapture();
-                    while (running)
+                    lock (captureLock)
                     {
-                        IplImage output;
-                        BayerTileFormat bayerTileFormat;
-                        camera.RetrieveBuffer(image);
-                        if (image.pixelFormat == PixelFormat.PixelFormatMono8 ||
-                            image.pixelFormat == PixelFormat.PixelFormatMono16 ||
-                            (image.pixelFormat == PixelFormat.PixelFormatRaw8 &&
-                               (image.bayerTileFormat == BayerTileFormat.None ||
-                                colorProcessing == ColorProcessingAlgorithm.NoColorProcessing)))
+                        ManagedCamera camera;
+                        using (var manager = new ManagedBusManager())
                         {
-                            unsafe
-                            {
-                                bayerTileFormat = image.bayerTileFormat;
-                                var depth = image.pixelFormat == PixelFormat.PixelFormatMono16 ? IplDepth.U16 : IplDepth.U8;
-                                var bitmapHeader = new IplImage(new Size((int)image.cols, (int)image.rows), depth, 1, new IntPtr(image.data));
-                                output = new IplImage(bitmapHeader.Size, bitmapHeader.Depth, bitmapHeader.Channels);
-                                CV.Copy(bitmapHeader, output);
-                            }
+                            var guid = manager.GetCameraFromIndex((uint)Index);
+                            camera = new ManagedCamera();
+                            camera.Connect(guid);
                         }
-                        else
+
+                        try
                         {
-                            unsafe
+                            var colorProcessing = ColorProcessing;
+                            using (var image = new ManagedImage())
                             {
-                                bayerTileFormat = BayerTileFormat.None;
-                                output = new IplImage(new Size((int)image.cols, (int)image.rows), IplDepth.U8, 3);
-                                using (var convertedImage = new ManagedImage(
-                                    (uint)output.Height,
-                                    (uint)output.Width,
-                                    (uint)output.WidthStep,
-                                    (byte*)output.ImageData.ToPointer(),
-                                    (uint)(output.WidthStep * output.Height),
-                                    PixelFormat.PixelFormatBgr))
+                                camera.StartCapture();
+                                while (!cancellationToken.IsCancellationRequested)
                                 {
-                                    convertedImage.colorProcessingAlgorithm = colorProcessing;
-                                    image.Convert(PixelFormat.PixelFormatBgr, convertedImage);
+                                    IplImage output;
+                                    BayerTileFormat bayerTileFormat;
+                                    camera.RetrieveBuffer(image);
+                                    if (image.pixelFormat == PixelFormat.PixelFormatMono8 ||
+                                        image.pixelFormat == PixelFormat.PixelFormatMono16 ||
+                                        (image.pixelFormat == PixelFormat.PixelFormatRaw8 &&
+                                           (image.bayerTileFormat == BayerTileFormat.None ||
+                                            colorProcessing == ColorProcessingAlgorithm.NoColorProcessing)))
+                                    {
+                                        unsafe
+                                        {
+                                            bayerTileFormat = image.bayerTileFormat;
+                                            var depth = image.pixelFormat == PixelFormat.PixelFormatMono16 ? IplDepth.U16 : IplDepth.U8;
+                                            var bitmapHeader = new IplImage(new Size((int)image.cols, (int)image.rows), depth, 1, new IntPtr(image.data));
+                                            output = new IplImage(bitmapHeader.Size, bitmapHeader.Depth, bitmapHeader.Channels);
+                                            CV.Copy(bitmapHeader, output);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        unsafe
+                                        {
+                                            bayerTileFormat = BayerTileFormat.None;
+                                            output = new IplImage(new Size((int)image.cols, (int)image.rows), IplDepth.U8, 3);
+                                            using (var convertedImage = new ManagedImage(
+                                                (uint)output.Height,
+                                                (uint)output.Width,
+                                                (uint)output.WidthStep,
+                                                (byte*)output.ImageData.ToPointer(),
+                                                (uint)(output.WidthStep * output.Height),
+                                                PixelFormat.PixelFormatBgr))
+                                            {
+                                                convertedImage.colorProcessingAlgorithm = colorProcessing;
+                                                image.Convert(PixelFormat.PixelFormatBgr, convertedImage);
+                                            }
+                                        }
+                                    }
+
+                                    observer.OnNext(new FlyCaptureDataFrame(output, image.imageMetadata, bayerTileFormat));
                                 }
                             }
                         }
-
-                        observer.OnNext(new FlyCaptureDataFrame(output, image.imageMetadata, bayerTileFormat));
+                        finally
+                        {
+                            camera.StopCapture();
+                            camera.Disconnect();
+                            camera.Dispose();
+                        }
                     }
-                });
-
-                thread.Start();
-                return () =>
-                {
-                    running = false;
-                    if (thread != Thread.CurrentThread) thread.Join();
-                    camera.StopCapture();
-                    image.Dispose();
-                    camera.Disconnect();
-                };
+                },
+                cancellationToken,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
             })
             .PublishReconnectable()
             .RefCount();
